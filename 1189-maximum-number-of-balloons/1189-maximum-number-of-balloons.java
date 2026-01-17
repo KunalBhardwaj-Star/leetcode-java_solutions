4af96c855af04dbae624e5725a62aa407c1c0145
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-01-17 19:36:26 +0530

    Time: 11 ms (28.31%), Space: 44.1 MB (28.93%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,22 +1,15 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
         HashMap<Character , Integer>count = new HashMap<>();
-        String target = "balloon";
-        int sum = Integer.MAX_VALUE , len = text.length();
-        for(int i = 0 ; i < len; i++){
-            char ch = text.charAt(i);
 
-            if(target.indexOf(ch) != -1) {
-                count.put(ch , count.getOrDefault(ch,0) + 1);
-            }
+        for(char ch : text.toCharArray()){
+            count.put(ch , count.getOrDefault(ch , 0) + 1);
         }
-        
-        for(int i = 0 ; i < 7 ; i++){
-            char curr = target.charAt(i);
-            if(!count.containsKey(curr) || (curr == 'l' && count.get(curr) < 2) || (curr == 'o' && count.get(curr) < 2)) return 0;
-            if(curr == 'l' || curr == 'o')sum = Math.min(sum , count.get(curr)/2);
-            else sum = Math.min(sum , count.get(curr));
+
+        if(count.containsKey('b') && count.containsKey('a') && count.get('l') >= 2 && count.get('o') >= 2 && count.containsKey('n')){
+            int a = Math.min(count.get('b') , Math.min(count.get('a') , Math.min(count.get('l')/2 , Math.min(count.get('o')/2 , count.get('n')))));
+            return a;
         }
-        return sum;
+        return 0;
     }
 }
