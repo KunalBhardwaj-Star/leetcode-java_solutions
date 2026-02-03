881b1a433d7dc1b0a5e4d568bbf5553ab7b507f4
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-02-03 22:50:43 +0530

    Time: 1 ms (99.61%), Space: 46.7 MB (80.81%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(int i  = 1; i <= n ; i++){
+            if(i % 3 == 0 && i % 5 == 0)ans.add("FizzBuzz");
+            else if(i % 3 == 0) ans.add("Fizz");
+            else if(i % 5 == 0)ans.add("Buzz");
+            else{
+                String s = String.valueOf(i);
+                ans.add(s);
+            }
+        }
+        return ans;
+    }
+}
