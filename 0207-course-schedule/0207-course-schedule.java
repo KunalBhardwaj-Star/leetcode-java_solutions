998e682e948783fb8d70020646ea6e837cc8d8d5
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-02-26 21:49:21 +0530

    Time: 6 ms (75%), Space: 47.3 MB (34.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,38 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adj = new ArrayList[numCourses];
-        int[] inDegree = new int[numCourses];
+        int len = prerequisites.length;
 
-        for (int i = 0; i < numCourses; i++) adj[i] = new ArrayList<>();
+        if(numCourses == 1) return true;
 
-        for (int[] pre : prerequisites) {
-            int course = pre[0];
-            int prerequisite = pre[1];
-            adj[prerequisite].add(course); 
-            inDegree[course]++;            
+        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+        int[] incoming = new int[numCourses];
+
+        for(int i = 0 ; i < numCourses ; i++)g.add(new ArrayList<>());
+
+        for(int i = 0 ; i < len ; i++){
+            int src = prerequisites[i][1] , des = prerequisites[i][0];
+            g.get(src).add(des);
+            incoming[des]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
-
+        
         for(int i = 0 ; i < numCourses ; i++){
-            if(inDegree[i] == 0) q.add(i);
+            if(incoming[i] == 0) q.add(i);
         }
 
         int count = 0;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            count++;
 
-            for(int neigh : adj[curr]){
-                inDegree[neigh]--;
-                if(inDegree[neigh] == 0){
-                    q.add(neigh);
-                }
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            count++;
+            for(int neigh : g.get(curr)){
+                incoming[neigh]--;
+                if(incoming[neigh] == 0)q.add(neigh);
             }
         }
 
         return count == numCourses;
+
     }
 }
