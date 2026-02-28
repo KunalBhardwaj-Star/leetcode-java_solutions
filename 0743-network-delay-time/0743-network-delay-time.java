144d6965fb53afc470ad8a378fc8f0120ea99241
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-02-28 19:33:12 +0530

    Time: 11 ms (69.19%), Space: 49 MB (67.11%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,65 +1,63 @@
 class Solution {
     public class Node implements Comparable<Node>{
-        int src , dis;
-        Node(int s , int dt){
+        int src , weig;
+        Node(int s , int w){
             this.src = s;
-            this.dis = dt;
+            this.weig = w;
         }
 
         @Override
-        public int compareTo(Node other) {
-            return this.dis - other.dis;
+        public int compareTo(Node x){
+            return this.weig - x.weig;
         }
     }
-
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<int[]>[] g = new ArrayList[n+1];
 
         for(int i = 0 ; i <= n ; i++) g[i] = new ArrayList<int[]>();
 
-        for(int i = 0 ; i < times.length; i++){
-            int src = times[i][0] , des = times[i][1] , w = times[i][2];
+        for(int[] neigh : times){
+            int src = neigh[0] , des = neigh[1] , w = neigh[2];
+
             int[] x = {des , w};
+
             g[src].add(x);
         }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
 
         int[] dist = new int[n+1];
         boolean[] vis = new boolean[n+1];
 
-        for(int i = 1; i <= n ; i++) if(i != k) dist[i] = Integer.MAX_VALUE;
+        for(int i = 0 ; i <= n ; i++) dist[i] = i == k ? 0 : Integer.MAX_VALUE;
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        Node src = new Node(k , 0);
-        pq.add(src);
+        Node start = new Node(k , 0);
+        pq.add(start);
 
         while(!pq.isEmpty()){
             Node curr = pq.remove();
             int u = curr.src;
+            vis[u] = true;
 
-            if(vis[u]) continue;
-            else {
-                vis[u] = true;
-                for(int[] neigh : g[u]){
-                int v = neigh[0] , w = neigh[1];
-                if(dist[u] + w < dist[v]){
-                    dist[v] = dist[u] + w;
-                    Node inp = new Node(v , dist[v]);
-                    pq.add(inp);
+            for(int[] neigh : g[u]){
+                int v = neigh[0] , dt = neigh[1];
+
+                if(dist[u] + dt < dist[v]){
+                    dist[v] = dist[u] + dt;
+
+                    Node next = new Node(v , dist[v]);
+                    pq.add(next);
                 }
-              }
             }
         }
 
-        int minTime = 0;
+        int delay = 0;
+
         for(int i = 1 ; i <= n ; i++){
-            if(dist[i] == Integer.MAX_VALUE){
-                System.out.println(i);
-                return -1;
-            }
-            minTime = Math.max(dist[i] , minTime);
+            if(dist[i] == Integer.MAX_VALUE) return -1;
+            delay = Math.max(delay , dist[i]);
         }
 
-        return minTime;
+        return delay;
     }
 }
