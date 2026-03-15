536a8fb5e3ba6acae59e4605eae5ab3a67f0281b
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-03-15 11:11:30 +0530

    Time: 46 ms (84.48%), Space: 124.5 MB (58.62%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.java
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -1,39 +1,59 @@
 class Fancy {
-    int [] arr;
-    int i ;
+
+    static final long MOD = 1000000007;
+
+    List<Long> arr;
+    long mul;
+    long add;
 
     public Fancy() {
-        arr = new int[100001];
-        Arrays.fill(arr , -1);
-        i = 0;
+        arr = new ArrayList<>();
+        mul = 1;
+        add = 0;
     }
-    
+
     public void append(int val) {
-        arr[i++] = val;
+
+        long x = (val - add + MOD) % MOD;
+        x = x * modInverse(mul) % MOD;
+
+        arr.add(x);
     }
-    
+
     public void addAll(int inc) {
-        for(int j = 0 ; j < i ; j++){
-            arr[j] += inc;
-        }
+        add = (add + inc) % MOD;
     }
-    
+
     public void multAll(int m) {
-        for(int j = 0 ; j < i ; j++){
-            arr[j] *= m;
-        }
+        mul = (mul * m) % MOD;
+        add = (add * m) % MOD;
     }
-    
-    public int getIndex(int idx){ 
-        return arr[idx] % (1000000007);
+
+    public int getIndex(int idx) {
+
+        if(idx >= arr.size()) return -1;
+
+        long val = arr.get(idx);
+        return (int)((val * mul % MOD + add) % MOD);
+    }
+
+    private long modInverse(long x) {
+        return power(x, MOD-2);
+    }
+
+    private long power(long a, long b) {
+
+        long res = 1;
+
+        while(b > 0){
+
+            if((b & 1) == 1)
+                res = res * a % MOD;
+
+            a = a * a % MOD;
+            b >>= 1;
+        }
+
+        return res;
     }
 }
-
-/**
- * Your Fancy object will be instantiated and called as such:
- * Fancy obj = new Fancy();
- * obj.append(val);
- * obj.addAll(inc);
- * obj.multAll(m);
- * int param_4 = obj.getIndex(idx);
- */
