1c1c7d1fc9391da3d166f67e5e98a7eedc19827f
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-03-22 09:41:53 +0530

    Time: 0 ms (100%), Space: 43.6 MB (81.83%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        rotate90(matrix);
+    }
+
+    private void rotate90(int[][] mat){
+        //transpose
+        int n = mat.length;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        //reverse
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n/2 ; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-j-1];
+                mat[i][n-j-1] = temp;
+            }
+        }
+    }
+}
