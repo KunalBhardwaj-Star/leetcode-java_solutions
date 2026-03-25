bae8ba30716925d8c9eb032c66993038ee8ebaeb
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-03-26 00:58:26 +0530

    Time: 14 ms (14.63%), Space: 91.1 MB (87.81%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.java
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -1,34 +1,31 @@
 class Solution {
     public int[][] constructProductMatrix(int[][] grid) {
-        int n = grid.length, m = grid[0].length;
-        int mod = 12345;
+        int n = grid.length , m = grid[0].length;
+        int[] arr = new int[n * m];
+        int MOD = 12345;
 
-        int total = n * m;
-        int[] arr = new int[total];
-
-        // Flatten grid
-        for (int i = 0; i < total; i++) {
-            arr[i] = grid[i / m][i % m] % mod;
+        for(int i = 0 ; i < n * m ; i++){
+            arr[i] = grid[i/m][i%m] % MOD;
         }
 
-        int[] prefix = new int[total];
-        int[] suffix = new int[total];
+        int[] prefix = new int[n * m];
+        int[] suffix = new int[n * m];
 
         prefix[0] = 1;
-        for (int i = 1; i < total; i++) {
-            prefix[i] = (int)((long)prefix[i - 1] * arr[i - 1] % mod);
+
+        for(int i = 1 ; i < n * m ; i++){
+            prefix[i] = (int)((long)prefix[i-1] * arr[i-1] % MOD) ;
         }
 
-        suffix[total - 1] = 1;
-        for (int i = total - 2; i >= 0; i--) {
-            suffix[i] = (int)((long)suffix[i + 1] * arr[i + 1] % mod);
+        suffix[n * m - 1] = 1;
+        for(int i = n * m - 2 ; i >= 0 ; i--){
+            suffix[i] = (int)((long)suffix[i+1] * arr[i+1] % MOD);
         }
 
         int[][] result = new int[n][m];
 
-        for (int i = 0; i < total; i++) {
-            int val = (int)((long)prefix[i] * suffix[i] % mod);
-            result[i / m][i % m] = val;
+        for(int i = 0 ; i < n * m ; i++){
+            result[i/m][i%m] = (int)((long)prefix[i] * suffix[i] % MOD);
         }
 
         return result;
