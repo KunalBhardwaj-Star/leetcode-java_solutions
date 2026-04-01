1b62e26f0889c65ea8cb187204e17c51ff3a1118
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-04-01 23:54:36 +0530

    Time: 15 ms (19.78%), Space: 48.6 MB (10.68%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,44 +1,31 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        if(s1.equals(s2)) return true;
+        int len = s1.length();
 
-        int n = s1.length();
-
-        char[] even1 = new char[(n+1)/2];
-        char[] even2 = new char[(n+1)/2];
-        char[] odd1 = new char[n/2];
-        char[] odd2 = new char[n/2];
+        char[] even1 = new char[(len + 1) / 2];
+        char[] even2 = new char[(len + 1) / 2];
+        char[] odd1 = new char[len / 2];
+        char[] odd2 = new char[len / 2];
 
         int e = 0 , o = 0;
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < len ; i++){
             if(i % 2 == 0){
                 even1[e] = s1.charAt(i);
                 even2[e] = s2.charAt(i);
                 e++;
             }
-            else {
+            else{
                 odd1[o] = s1.charAt(i);
                 odd2[o] = s2.charAt(i);
                 o++;
             }
         }
 
-        // for(int i = 0 ; i < n / 2; i++){
-        //     System.out.println("Even : " + even1[i] + " " + even2[i]);
-        //     System.out.println("Odd : " + odd1[i] + " " + odd2[i]);
-        // }
-
         Arrays.sort(even1);
         Arrays.sort(even2);
         Arrays.sort(odd1);
         Arrays.sort(odd2);
-
-        // for(int i = 0 ; i < n / 2; i++){
-        //     System.out.println("Even : " + even1[i] + " " + even2[i]);
-        //     System.out.println("Odd : " + odd1[i] + " " + odd2[i]);
-        // }
-
 
         return Arrays.equals(even1 , even2) && Arrays.equals(odd1 , odd2);
     }
