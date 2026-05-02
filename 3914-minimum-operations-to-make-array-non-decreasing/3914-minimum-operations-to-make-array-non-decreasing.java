4cf4162bb2604b3a55ea5d8bbb19bae4cce17142
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-02 23:44:21 +0530

    Time: 3 ms (98.88%), Space: 87.1 MB (96.81%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.java
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.java
@@ -1,12 +1,9 @@
 class Solution {
     public long minOperations(int[] nums) {
         int len = nums.length;
-        long ans = 0;
+        long count = 0 ;
+        for(int i = 1 ; i < len ; i++) count += nums[i] < nums[i-1] ? nums[i - 1] - nums[i] : 0;
 
-        for(int i = 0 ; i < len - 1 ; i++){
-            if(nums[i]  > nums[i + 1] ) ans += nums[i] - nums[i + 1];
-        }
-
-        return ans;
+        return count;
     }
 }
