4dbe3de14d96661128b11d43979deef3774cbb15
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-06 23:54:55 +0530

    Time: 3 ms (93.82%), Space: 42.1 MB (72.36%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -1,25 +1,27 @@
 class Solution {
-    private boolean goodOrNot(int x) {
-        boolean changed = false;
+    private boolean goodNumOrNot(int n) {
+        boolean good_int = false;
+
+        int x = n;
+
         while (x != 0) {
             int curr = x % 10;
-            if (curr == 3 || curr == 4 || curr == 7)
+            if (curr == 3 || curr == 7 || curr == 4)
                 return false;
-
-            if (curr == 2 || curr == 5 || curr == 6 || curr == 9) {
-                changed = true;
-            }
+            if (curr == 2 || curr == 5 || curr == 6 || curr == 9)
+                good_int = true;
 
             x /= 10;
         }
-        return changed;
+
+        return good_int;
     }
 
     public int rotatedDigits(int n) {
         int count = 0;
-        for (int i = 2; i <= n; i++) {
-            if (goodOrNot(i))
-                count++;
+
+        for(int i = 2 ; i <= n ; i++){
+            if(goodNumOrNot(i)) count++;
         }
 
         return count;
