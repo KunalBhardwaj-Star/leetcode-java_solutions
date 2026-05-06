108a17d3c0db8ab6cbf365fa29beacd1b65ac13b
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-06 22:46:18 +0530

    Time: 8 ms (59.38%), Space: 124.8 MB (43.27%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -15,13 +15,6 @@ class Solution {
             }
         }
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                System.out.print(boxGrid[i][j] + " ");
-            }
-            System.out.println();
-        }
-
         char[][] res = new char[m][n];
 
         for(int i = 0 ; i < n ; i++){
