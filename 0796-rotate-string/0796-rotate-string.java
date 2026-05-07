b01e4f3d4cad5a6f06fa7ed2db291a458da2187e
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-07 19:40:13 +0530

    Time: 63 ms (2.09%), Space: 43.1 MB (12.1%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,6 +3,16 @@ class Solution {
         if(s.length() != goal.length())
             return false;
         
-        return (s + s).contains(goal);
+        String con = s + s;
+
+        int len1 = con.length() , len2 = goal.length();
+
+        for(int i = 0; i < len1 - len2 ; i++){
+            String curr = con.substring(i , i + len2);
+
+            if(goal.equals(curr)) return true;
+        }
+
+        return false;
     }
 }
