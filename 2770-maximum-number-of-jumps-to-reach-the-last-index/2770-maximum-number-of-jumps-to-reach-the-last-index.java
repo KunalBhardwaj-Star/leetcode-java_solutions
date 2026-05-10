34de7caf451f7640d450452a09c6f67fc147093b
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-10 19:46:35 +0530

    Time: 13 ms (92.26%), Space: 46.7 MB (90.48%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,20 +1,25 @@
 class Solution {
     public int maximumJumps(int[] nums, int target) {
-        int n = nums.length;
+        int len = nums.length;
 
-        int dp[] = new int[n];
+        if (len == 0)
+            return 0;
+
+        int[] dp = new int[len];
+
         Arrays.fill(dp , -1);
+
         dp[0] = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if ((-target <= nums[j] - nums[i]) && (nums[j] - nums[i] <= target) && dp[i] != -1) {
-                    dp[j] = Math.max(dp[j], dp[i] + 1);
-                }
+        for (int i = 1; i < len; i++) {
+            for(int j = 0 ; j  < i ; j++){
+                int diff = nums[j] - nums[i];
 
+                if(dp[j] != -1 && diff <= target && -target <= diff)
+                    dp[i] = Math.max(dp[i] , dp[j] + 1);
             }
         }
 
-        return dp[n - 1];
+        return dp[len - 1] ;
     }
 }
