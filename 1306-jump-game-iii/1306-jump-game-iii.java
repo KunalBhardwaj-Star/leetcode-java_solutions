2e60d0a6091c8d56eada65ea888cdc75572f423a
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-05-18 19:35:51 +0530

    Time: 2 ms (82.5%), Space: 53 MB (24.32%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,29 +1,23 @@
 class Solution {
-
-    public boolean dfs(int[] arr, int start, boolean[] visited) {
-
-        if (start < 0 || start >= arr.length) {
+    private boolean dfs(int[] arr , int start , boolean[] vis){
+        if(start < 0 || start >= arr.length)
             return false;
-        }
 
-        if (visited[start]) {
+        if(vis[start])
             return false;
-        }
 
-        if (arr[start] == 0) {
+        if(arr[start] == 0)
             return true;
-        }
 
-        visited[start] = true;
+        vis[start] = true;
 
-        return dfs(arr, start + arr[start], visited)
-                || dfs(arr, start - arr[start], visited);
+        return dfs(arr , arr[start] + start , vis) || dfs(arr , start - arr[start] , vis);
     }
-
     public boolean canReach(int[] arr, int start) {
+        int len = arr.length;
 
-        boolean[] visited = new boolean[arr.length];
+        boolean[] vis = new boolean[len];
 
-        return dfs(arr, start, visited);
+        return dfs(arr , start , vis);
     }
 }
