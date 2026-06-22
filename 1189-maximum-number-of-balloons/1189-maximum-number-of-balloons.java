316f7d20854158f033e1d964f3a0c35b2a66a2cc
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-06-22 10:02:15 +0530

    Time: 10 ms (43.98%), Space: 44.4 MB (8.64%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character , Integer>count = new HashMap<>();
+        HashMap<Character, Integer> count = new HashMap<>();
+        for (char ch : text.toCharArray())
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
 
-        for(char ch : text.toCharArray()){
-            count.put(ch , count.getOrDefault(ch , 0) + 1);
-        }
-
-        if(count.containsKey('b') && count.containsKey('a') && count.get('l') >= 2 && count.get('o') >= 2 && count.containsKey('n')){
-            int a = Math.min(count.get('b') , Math.min(count.get('a') , Math.min(count.get('l')/2 , Math.min(count.get('o')/2 , count.get('n')))));
+        if (count.containsKey('b') && count.containsKey('a') && count.get('l') >= 2 && count.get('o') >= 2
+                && count.containsKey('n')) {
+            int a = Math.min(count.get('b'), Math.min(count.get('a'),
+                    Math.min(count.get('l') / 2, Math.min(count.get('o') / 2, count.get('n')))));
             return a;
         }
+
         return 0;
     }
 }
