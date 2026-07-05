37a932f92967faa0e05107e4a21d3636be17be8f
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-07-05 23:53:45 +0530

    Time: 77 ms (24.88%), Space: 169.1 MB (21.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,16 @@
 class Solution {
     private int lcs(int[] arr1 , int[] arr2){
-        int n = arr1.length;
-        int m = arr2.length;
+        int n = arr1.length , m = arr2.length;
 
         int[][] dp = new int[n+1][m+1];
 
         for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(arr1[i-1] == arr2[j-1])
+            for(int j = 1; j <= m ; j ++){
+                if(arr1[i-1] == arr2[j-1]){
                     dp[i][j] = dp[i-1][j-1] + 1;
-
-                else 
+                } else {
                     dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                }
             }
         }
 
@@ -20,16 +19,16 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
 
-        for(int x : nums)
+        for(int x : nums){
             set.add(x);
+        }
 
-        int arr2[] = new int[set.size()];
+        int[] arr2 = new int[set.size()];
 
         int i = 0;
 
         for(int n : set){
-            arr2[i] = n;
-            i++;
+            arr2[i++] = n;
         }
 
         Arrays.sort(arr2);
