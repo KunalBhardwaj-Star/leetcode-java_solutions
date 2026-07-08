e471763deff31a46c8517135dd2c234727df4c03
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-07-09 02:16:07 +0530

    Time: 1 ms (80.84%), Space: 43.2 MB (48.98%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -5,23 +5,19 @@ class Solution {
         if(n == 0 || s.charAt(0) == '0')
             return 0;
 
-        int prev2 = 1;
-        int prev1 = 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
 
-        for(int i = 1 ; i < n ; i++){
-            int curr = 0;
-
+        for(int i = 1; i < n ; i++){
             if(s.charAt(i) >= '1' && s.charAt(i) <= '9')
-                curr += prev1;
+                dp[i] += dp[i-1];
 
             int twoDig = Integer.parseInt(s.substring(i-1 , i+1));
-            if(twoDig >= 10 && twoDig <= 26)
-                curr += prev2;
 
-            prev2 = prev1;
-            prev1 = curr;
+            if(twoDig >= 10 && twoDig <= 26)
+                dp[i] += i > 1 ? dp[i-2] : dp[i-1];
         }
 
-        return prev1;
+        return dp[n-1];
     }
 }
