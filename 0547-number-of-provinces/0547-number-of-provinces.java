414fb922198036a2cd783340b31a6d49425f869e
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-07-14 13:53:34 +0530

    Time: 2 ms (42.98%), Space: 47 MB (88.12%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,63 @@
 class Solution {
-    private void dfs(int idx , int[][] isConnected , boolean[] vis){
-        for(int i = 0 ; i < isConnected[idx].length ; i++){
-            if(isConnected[idx][i] == 1 && !vis[i]){
-                vis[i] = true;
-                dfs(i , isConnected , vis);
+    private class UnionFind{
+        private int[] parents;
+        private int[] rank;
+        private int components;
+
+        public UnionFind(int n){
+            parents = new int[n];
+            rank = new int[n];
+            components = n;
+
+            for(int i = 0; i < n ; i++){
+                parents[i] = i;
             }
+        }
+
+        private int find(int x){
+            if(parents[x] != x){
+                parents[x] = find(parents[x]);
+            }
+
+            return parents[x];
+        }
+
+        private boolean union(int x , int y){
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if(rootX == rootY)
+                return false;
+            
+            if(rank[rootX] > rank[rootY]){
+                parents[rootY] = rootX;
+            } else if(rank[rootX] < rank[rootY]){
+                parents[rootX] = rootY;
+            } else{
+                parents[rootY] = rootX;
+                rank[rootX]++;
+            }
+
+            components--;
+            return true;
+        }
+
+        public int getComponents(){
+            return components;
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] vis = new boolean[n];
-
-        int count = 0;
+        UnionFind uf = new UnionFind(n);
 
         for(int i = 0 ; i < n ; i++){
-            if(!vis[i]){
-                dfs(i , isConnected , vis);
-                count++;
+            for(int j = 0 ; j < n ; j++){
+                if(isConnected[i][j] == 1){
+                    uf.union(i , j);
+                }
             }
         }
 
-        return count;
+        return uf.getComponents();
     }
 }
