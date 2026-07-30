62dd11f26c9e7ee0e531cf3de74ac1d4d48021fe
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-07-30 18:08:31 +0530

    Time: 52 ms (31.06%), Space: 47.5 MB (49.18%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length() , m = p.length();
-        boolean[][] dp = new boolean[n+1][m+1];
+        int len1 = s.length();
+        int len2 = p.length();
+
+        boolean[][] match = new boolean[len1 + 1][len2 + 1];
 
         //initialization
-        dp[0][0] = true;
-        //for column = 0 and varying row we always get false bcz if pattern len = 0 then for any len of string we get false --> no need to initialize (i , 0) always 0.
-        for(int i = 1; i < n+1 ; i++){
-            dp[i][0] = false;
-        }
-        for(int j = 1 ; j < m+1 ; j++){
-            if(p.charAt(j-1) == '*'){ dp[0][j] =dp[0][j-1];}
-        }
+        match[0][0] = true;
+        for(int i = 1 ; i <= len1 ; i++)
+            match[i][0] = false;
 
-        //bottom-up-manner
-        for(int i = 1; i < n+1 ; i++){
-            for(int j = 1; j < m+1 ; j++){
-                //if characters are same or pattern character is ?
-                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }else if(p.charAt(j-1) == '*'){ //if character in p is *
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                } else{ //None of the above case
-                    dp[i][j] = false;
-                }
+        for(int j = 1 ; j <= len2 ; j++)
+            match[0][j] = p.charAt(j-1) == '*' ? match[0][j-1] : false;
+
+        for(int i = 1 ; i <= len1 ; i++){
+            for(int j = 1 ; j <= len2 ; j++){
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?')
+                    match[i][j] = match[i-1][j-1];
+
+                else if (p.charAt(j-1) == '*')
+                    match[i][j] = match[i-1][j] || match[i][j-1];
+
+                else 
+                    match[i][j] = false;
             }
         }
-        return dp[n][m];
+
+        return match[len1][len2];
     }
 }
