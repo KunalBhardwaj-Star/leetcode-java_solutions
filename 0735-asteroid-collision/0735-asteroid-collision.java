93a1de2cd0de370391cc2dbb93c48bd3bb8e26c6
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-08-11 00:41:49 +0530

    Time: 4 ms (83.22%), Space: 47.1 MB (44.5%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,43 +1,39 @@
-import java.util.*;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        
         Stack<Integer> s = new Stack<>();
+
         s.push(asteroids[0]);
 
-        for(int i = 1 ; i < asteroids.length ; i++){
+        int n = asteroids.length;
+
+        for(int i = 1; i < n ; i++){
             boolean flag = true;
 
             while(!s.isEmpty() && asteroids[i] < 0 && s.peek() > 0){
-                if(asteroids[i] < 0){
-                    if(Math.abs(asteroids[i]) > s.peek()){
-                        s.pop();
-                        continue;
-                    }
-                    else if(Math.abs(asteroids[i]) == s.peek()){
-                        s.pop();
-                        flag = false;
-                        break;
-                    } else {
-                        flag = false;
-                        break;
-                    }
-                } else {
-                    s.push(asteroids[i]);
+                if(Math.abs(asteroids[i]) > s.peek()){
+                    s.pop();
+                    continue;
+                } else if(Math.abs(asteroids[i]) == s.peek()){
+                    s.pop();
+                    flag = false;
+                    break;
+                } else{
+                    flag = false;
+                    break;
                 }
             }
 
             if(flag){
-               s.push(asteroids[i]); 
+                s.push(asteroids[i]);
             }
         }
 
-        int[] astCol = new int[s.size()];
-        
-        for(int i = s.size()-1 ; i >= 0 ; i--){
-            astCol[i] = s.pop();
+        int[] ans = new int[s.size()];
+
+        for(int i = ans.length - 1 ; i >= 0 ; i--){
+            ans[i] = s.pop();
         }
-        return astCol;
+
+        return ans;
     }
 }
