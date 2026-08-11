059d082b3d19c4e18c328a296e89c78e30b02c4c
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-08-11 16:27:14 +0530

    Time: 153 ms (92.08%), Space: 35.1 MB (54.49%) - LeetHub

--- 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.py
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.py
@@ -1,22 +1,14 @@
 class Solution:
     def minPrice(self, prices: list[int], discounts: list[int]) -> float:
-        prices.sort()
-        discounts.sort()
-
-        p1 = len(prices) - 1
-        p2 = len(discounts) - 1
+        prices.sort(reverse = True)
+        discounts.sort(reverse = True)
 
         ans = 0.0
 
-        while p1 >= 0 and p2 >= 0:
-            dis = (100 - discounts[p2]) / 100
-            ans += (prices[p1] * dis)
-            p1 -= 1
-            p2 -= 1
+        for i in range(0 , min(len(prices) , len(discounts))):
+            ans += prices[i] * (100 - discounts[i]) / 100
 
-        while p1 >= 0:
-            ans += prices[p1]
-            p1 -= 1
-
-        return ans
+        for i in range(len(discounts) , len(prices)):
+            ans += prices[i]
         
+        return ans
