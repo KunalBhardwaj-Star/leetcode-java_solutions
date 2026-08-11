3c4c21bb2d2ec61757dcf41aaf016e24dc188ffa
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-08-11 15:45:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (34.55%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def __init__(self):
+        self.ans = []
+
+    def backtrack(self , n: int , open_count: int , close_count: int , path):
+        if (len(path) == 2 * n) & (open_count == close_count):
+            self.ans.append("".join(path))
+            return
+
+        if open_count < n:
+            path.append('(')
+            self.backtrack(n , open_count + 1 , close_count , path)
+            path.pop()
+
+        if close_count < open_count:
+            path.append(')')
+            self.backtrack(n , open_count , close_count + 1 , path)
+            path.pop()
+
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.backtrack(n , 0 , 0 , [])
+        return self.ans
