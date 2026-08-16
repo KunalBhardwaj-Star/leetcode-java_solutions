650fc838ceb4149db01714b5243e77ac70e8b0ca
Kunal Bhardwaj <bhardwajkunal399@gmail.com>
2026-08-16 14:56:20 +0530

    Time: 7 ms (29.05%), Space: 115 MB (31.76%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0 = 0 , c1 = 0 , c2 = 0;
+
+        for(int i = 0 ; i < stones.length ; i++){
+            if(stones[i] % 3 == 0)
+                c0++;
+
+            else if(stones[i] % 3 == 1)
+                c1++;
+
+            else 
+                c2++;
+        }
+
+        if(c0 % 2 == 0)
+            return c1 > 0 && c2 > 0;
+
+        return Math.abs(c1 - c2) > 2;
+    }
+}
